3256574e2d2dff5a9d191339fd0452f92019e170
kcgcom <93268752+kcgcom@users.noreply.github.com>
2026-04-04 06:42:39 +0000

    fix: PostHog toolbar 스크립트 로드를 CSP에서 허용
    PostHog toolbar가 internal-j.posthog.com에서 config.js를 주입할 때
    script-src가 us/eu asset 호스트만 허용해 스크립트 로드가 차단됐다.
    PostHog Cloud의 호스트 변형을 포괄하도록 *.posthog.com을 script-src에
    추가해 toolbar 초기화가 이어질 수 있게 했다.
    
    Constraint: PostHog toolbar는 asset 호스트 외에 Cloud 내부 호스트에서 스크립트를 로드할 수 있음
    Constraint: CSP 수정은 배포 후 응답 헤더에 반영되어야 브라우저 차단이 해소됨
    Rejected: internal-j.posthog.com만 개별 허용 | 다른 PostHog 내부 호스트 변형에서 다시 깨질 수 있음
    Rejected: script-src를 과도하게 완화 | 불필요하게 공격 표면을 넓힘
    Confidence: high
    Scope-risk: narrow
    Directive: 외부 스크립트 차단 이슈는 실제 차단된 호스트를 확인한 뒤 공급자 단위 허용 범위를 최소로 추가할 것
    Tested: pnpm lint; pnpm build
    Not-tested: 배포 환경에서 toolbar 실행 후 브라우저 콘솔 수동 확인

--- next.config.ts
+++ next.config.ts
@@ -25,7 +25,7 @@ const posthogConnectSrc = getCspOrigins(
 
 const CSP = [
   "default-src 'self'",
-  "script-src 'self' 'unsafe-inline' https://dapi.kakao.com https://t1.daumcdn.net https://apis.google.com https://www.gstatic.com https://www.googletagmanager.com https://static.cloudflareinsights.com https://us-assets.i.posthog.com https://eu-assets.i.posthog.com",
+  "script-src 'self' 'unsafe-inline' https://dapi.kakao.com https://t1.daumcdn.net https://apis.google.com https://www.gstatic.com https://www.googletagmanager.com https://static.cloudflareinsights.com https://us-assets.i.posthog.com https://eu-assets.i.posthog.com https://*.posthog.com",
   "style-src 'self' 'unsafe-inline'",
   "img-src 'self' data: blob: https://*.daumcdn.net https://*.kakaocdn.net",
   [
